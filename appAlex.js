26f70fc9ee4fe8e699aba3f82baf693fd547d7ac
Alex Ironside <aleksander3wielki@gmail.com>
2017-11-29 15:18:44 -0500

    Added content to index

--- appAlex.js
+++ appAlex.js
@@ -24,14 +24,14 @@ console.log(ipInfo);
 /**
  *  Fetching stuff. Does the same thing as getInfo();
  */
-function getInfo() {
-  fetch(Url) // Call the fetch function passing the url of the API as a parameter
-    .then(function (response) {
-      return response.json();
-    }).then(function (data) {
-      console.log(data);
-    })
-    .catch(function () {
-      // This is where you run code if the server returns any errors
-    });
-}
+// function getInfo() {
+//   fetch(Url) // Call the fetch function passing the url of the API as a parameter
+//     .then(function (response) {
+//       return response.json();
+//     }).then(function (data) {
+//       console.log(data);
+//     })
+//     .catch(function () {
+//       // This is where you run code if the server returns any errors
+//     });
+// }
